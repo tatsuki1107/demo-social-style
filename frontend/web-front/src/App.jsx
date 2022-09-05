cfc61bd0033864a412fd73131b2baf8f9c13856a
tatsuki1107 <tatsuki11070522@icloud.com>
2022-09-05 15:43:03 +0900

    create file directry and toppage

--- frontend/web-front/src/App.jsx
+++ frontend/web-front/src/App.jsx
@@ -1,10 +1,42 @@
 import './App.css';
 import React from 'react';
+import StartButton from './components/StartButton';
 
 const App = () => {
   return (
     <div className="App">
-      <p>App page!!</p>
+      <p>Header Component!!</p>
+
+      <div className='backImg'>
+        <div className='startButton-1'>
+          <StartButton />
+        </div>
+      </div>
+
+      <div className='explanation'>
+        <p>ソーシャルスタイルって<span className='emphasis'>ナニ？</span></p>
+
+        <p>ソーシャルスタイルとは11968年にアメリカの産業心理学者であるデビッド・メリル氏が提唱した<span className='emphasis'>コミュニケーション</span>コミュニケーションの理論です。
+          人の言動を4つのスタイルに分けて分析し、<span className='emphasis'>相手が望ましいと感じる対応を探し、選択する方法</span>として活用されています。
+
+          人の言動を単純に4つに分けることはできませんが、大まかな方向性としていずれかのスタイルに分類されるといわれています。</p>
+      </div>
+
+      <div className='graph'>
+        <div class="graf_top">
+          <div class="graf_elem tl"><div class="background_tl"></div></div>
+          <div class="graf_elem tr"><div class="background_tr"></div></div>
+        </div>
+        <div class="graf_btm">
+          <div class="graf_elem bl"><div class="background_bl"></div></div>
+          <div class="graf_elem br"><div class="background_br"></div></div>
+        </div>
+        <div class="triangle t_top"></div>
+        <div class="triangle t_right"></div>
+        <div class="triangle t_bottom"></div>
+        <div class="triangle t_left"></div>
+      </div>
+
     </div>
   );
 }
---
+++ frontend/web-front/src/components/StartButton/index.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import './index.css'
+
+const StartButton = () => {
+  return (
+    <button className="button">今すぐ診断する</button>
+  );
+};
+
+export default StartButton;
