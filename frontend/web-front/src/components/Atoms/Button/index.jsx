cd622be80857a785dd7d44d0aa203a45c4bfd8b7
root <root@Wths10712.localdomain>
2022-09-16 14:21:16 +0900

    ボタンアニメーションを追加

--- frontend/web-front/src/components/Atoms/Button/index.jsx
+++ frontend/web-front/src/components/Atoms/Button/index.jsx
@@ -12,6 +12,17 @@ filter: drop-shadow(0px 4px 4px rgba(0, 0, 0, 0.25));
 display: flex;
 border-radius: 20px;
 align-items: center;
+cursor: pointer;
+transition: background-color 0.75s ease-out;
+:hover{
+  background-color: #f1a35a;
+}
+:active{
+  -webkit-transform: translate(0,2px);
+	-moz-transform: translate(0,2px);
+	transform: translate(0,2px);
+	filter:none;
+}
 `;
 
 const Button = ({ onClick, children }) => {
