0964af3f77b1ea894ac4306c0015eafbf87f4dd3
tatsuki1107 <tatsuki11070522@icloud.com>
2022-10-11 13:37:31 +0900

    fix-cors-setting

--- auth_test/django_server/test_auth/config/settings.py
+++ auth_test/django_server/test_auth/config/settings.py
@@ -47,6 +47,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
 ]
 
 ROOT_URLCONF = 'config.urls'
@@ -118,3 +119,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
+
+CORS_ORIGIN_WHITELIST = (
+    'http://127.0.0.1:5500',
+)
