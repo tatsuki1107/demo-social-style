4fa042ac2dcf0b7f7f31fc5d15a6e207ad7ac915
tatsuki1107 <tatsuki11070522@icloud.com>
2022-10-21 18:03:07 +0900

    init.shにrect-loading追加

--- frontend/web-front/init.sh
+++ frontend/web-front/init.sh
@@ -2,3 +2,4 @@ npm install && npm upgrade --no-progress --network-timeout 1000000
 npm i @fortawesome/react-fontawesome && npm i @fortawesome/fontawesome-svg-core
 npm i react-router-dom
 npm install styled-components
+npm install react-loading
